b931b96feb18be26628aeeed98504c2ddcb501a1
adrian <adrian.miranda.r@gmail.com>
2021-04-19 01:28:25 -0400

    mesclando js con master , entre subRamas no se puede al parecer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
 alert("JS Funcionando");
 document.write("Probando el DOM");
+document.write("Esta es la Impresion desde la rama JS");
 console.log("La consola funciona");
+console.log(" segunda prueba merge entre ramas js y html");
